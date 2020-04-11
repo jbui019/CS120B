0eedf6594b37440ac340540052ce8a0dd2c07129
Jarvis Bui <jbui019@wch136-30.cs.ucr.edu>
2020-04-10 18:45:44 -0700

    finished

---
+++ turnin/jbui019_lab2_part4.c
@@ -0,0 +1,73 @@
+/*
+	Author: jbui019
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; //INPUT A
+	DDRB = 0x00; PORTB  = 0xFF; //INPUT B
+	DDRC = 0x00; PORTC = 0xFF; //INPUT C
+	DDRD = 0xFF; PORTD = 0x00; // OUTPUT D
+    /* Insert your solution below */
+	unsigned char tmpB = 0x00;
+        unsigned char tmpA = 0x00;
+        unsigned char tmpC = 0x00;
+
+    while (1) {
+	// Read Input from A B C
+        unsigned char totalWeight = 0x00;
+        unsigned char tmpD = 0x00;
+	unsigned char ACweight = 0x00; 
+	unsigned char heavierAC = 0x00;
+	unsigned char lighterAC = 0x00;
+	tmpA = PINA;
+	tmpB = PINB;
+	tmpC = PINC; 
+	
+	// add together weight
+	totalWeight = tmpB + tmpA + tmpC;
+	tmpD = totalWeight;
+
+	if(tmpA > tmpC){
+		heavierAC = tmpA;
+		lighterAC = tmpC;
+	}
+	else{
+		heavierAC = tmpC;
+		lighterAC = tmpA;
+	}
+
+	// if total weight is greater than 140kg set PD0 to 1
+	if(totalWeight > 0x8C ){
+		tmpD = tmpD | 0x01;
+
+	}
+	else{
+		tmpD = tmpD & ~(0x01);
+	}
+
+	if((heavierAC  -  lighterAC) > 0x50 ){
+		tmpD = tmpD | (0x01 << 1);
+
+	}
+	else{
+		tmpD = tmpD & ~(0x01 << 1);
+	}
+
+	PORTD = tmpD;
+	}
+	
+    return 1;
+}
+
